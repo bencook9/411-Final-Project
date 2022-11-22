eb36fc1ef549f8612c304a99fa6cfdbddc9bebe7
Benjamin Cook <90069170+bencook9@users.noreply.github.com>
2022-11-22 12:29:08 -0600

    Successful test connection to XAMPP database

--- Movies Finder/main.py
+++ Movies Finder/main.py
@@ -1,1 +1,21 @@
 # Project created by: Ben Cook, Erik Konnath, James Coddington, Viet Ninh
+# pip install mysql-connector, pip install wheel
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "test"
+)
+
+cursor = connection.cursor()
+
+query1 = "select * from test"
+
+cursor.execute(query1)
+
+table = cursor.fetchall()
+
+for row in table:
+    print(row)
