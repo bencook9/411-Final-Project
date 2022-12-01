4524e5739bfd5e3ac5c42f3c0ce91f111ac1ed2a
Benjamin Cook <90069170+bencook9@users.noreply.github.com>
2022-12-01 12:01:59 -0600

    Update main.py

--- Movies Finder/main.py
+++ Movies Finder/main.py
@@ -2,8 +2,7 @@
 # pip install mysql-connector, pip install wheel
 import mysql.connector
 
-
-def find_platforms(movie_name):
+def database_connection():
     connection = mysql.connector.connect(
         host = "localhost",
         user = "root",
@@ -11,9 +10,16 @@ def find_platforms(movie_name):
         database = "movies"
     )
 
+    return connection
+    
+
+def find_platforms(movie_name):
+    connection = database_connection()
+
+    # cursor for executing queries to the database
     cursor = connection.cursor()
 
-    query = f"select platforms from platforms WHERE name = '{movie_name}'"
+    query = f"select platforms from movies WHERE name = '{movie_name}'"
     cursor.execute(query)
 
     table = cursor.fetchall()
@@ -26,10 +32,29 @@ def find_platforms(movie_name):
         return table[0][0]
 
 
-movie_name = input("What movie would you like to search for?\n")
+def find_movies(platform_name):
+    pass
 
-platforms = find_platforms(movie_name)
 
-if platforms:
-    platforms = platforms.replace(",", "\n")
-    print(f"The movie {movie_name} can be found on: \n{platforms}")
+
+answer = input("Would you like to search for a movie? [y/n]")
+
+if answer.upper() == "Y":
+    movie_name = input("What movie would you like to search for?\n")
+    platforms = find_platforms(movie_name)
+
+    if platforms:
+        platforms = platforms.replace(",", "\n")
+        print(f"The movie {movie_name} can be found on: \n{platforms}")
+
+elif answer.upper() == "N":
+    new_answer = input("Would you like to see a catalogue of movies on a platform? [y/n]")
+    
+    if new_answer.upper() == "Y":
+        platform_name = input("What platform would you like to search for?\n")
+        movies = find_movies(platform_name)
+    
+    elif new_answer.upper() == "N":
+        print("Exiting the application...")
+else:
+    print("Input not recognized ending program")
