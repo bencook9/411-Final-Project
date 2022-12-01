4ec1fdc98bd50d53e02d2cfce4cfd6309a3f2194
vietninh <vietquocninh@gmail.com>
2022-12-01 13:03:40 -0600

    Created functionality to search for things
    Co-authored-by: Benjamin Cook <bencook9@users.noreply.github.com>

--- Movies Finder/main.py
+++ Movies Finder/main.py
@@ -2,24 +2,22 @@
 # pip install mysql-connector, pip install wheel
 import mysql.connector
 
-def database_connection():
-    connection = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = "",
-        database = "movies"
-    )
+connection = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = "movies_finder"
+)
 
-    return connection
+# cursor for executing queries to the database
+cursor = connection.cursor()
     
 
 def find_platforms(movie_name):
-    connection = database_connection()
 
-    # cursor for executing queries to the database
-    cursor = connection.cursor()
-
-    query = f"select platforms from movies WHERE name = '{movie_name}'"
+    query = f"""select platforms.platform_name from movies join on_platform on movies.movieId = on_platform.movie_id 
+                join platforms on platforms.platform_id = on_platform.platform_id 
+                where movies.name ='{movie_name}';"""
     cursor.execute(query)
 
     table = cursor.fetchall()
@@ -28,33 +26,43 @@ def find_platforms(movie_name):
     if not table:
         print(f"{movie_name} does not exist in our database")
     else:
-        # table is a list(tuple()) so we just want to return the values in the tuple
-        return table[0][0]
+        return table
 
 
 def find_movies(platform_name):
-    pass
+
+    query = f"""SELECT movies.name FROM movies
+                join on_platform on movies.movieId = on_platform.movie_id 
+                join platforms on platforms.platform_id = on_platform.platform_id 
+                where platforms.platform_name ='{platform_name}';"""
+    cursor.execute(query)
+
+    table = cursor.fetchall()
+    # check if platform has movies
+    if not table:
+        print(f"{platform_name} does not have any movies in our database")
+    else:
+        return table
 
 
+answer = input("Enter [m] if you want to search a movie. Enter [p] if you wish to search a platform:\n")
 
-answer = input("Would you like to search for a movie? [y/n]")
+match answer.upper():
+    case "M":
+        movie_name = input("What movie would you like to search for?\n")
+        # MAKE CALL TO METHOD movie_name.lower()
+        platforms = find_platforms(movie_name)
 
-if answer.upper() == "Y":
-    movie_name = input("What movie would you like to search for?\n")
-    platforms = find_platforms(movie_name)
-
-    if platforms:
-        platforms = platforms.replace(",", "\n")
-        print(f"The movie {movie_name} can be found on: \n{platforms}")
-
-elif answer.upper() == "N":
-    new_answer = input("Would you like to see a catalogue of movies on a platform? [y/n]")
-    
-    if new_answer.upper() == "Y":
+        if platforms:
+            print(platforms)
+        
+    case "P":
         platform_name = input("What platform would you like to search for?\n")
+        # MAKE CALL TO METHOD platform_name.lower()
         movies = find_movies(platform_name)
-    
-    elif new_answer.upper() == "N":
-        print("Exiting the application...")
-else:
-    print("Input not recognized ending program")
+        
+        if movies:
+            print(movies)
+
+    case _:
+        print("Input not recognized ending program")
