07b8e17e5ab4399b4b7772461552b582714467f3
vietninh <vietquocninh@gmail.com>
2022-12-04 22:24:04 -0600

    Pushing db file and some error handling

--- Movies Finder/main.py
+++ Movies Finder/main.py
@@ -1,17 +1,12 @@
 # Project created by: Ben Cook, Erik Konnath, James Coddington, Viet Ninh
 # pip install mysql-connector, pip install wheel
-import mysql.connector
+import sqlite3
 
-connection = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "",
-    database = "movies_finder"
-)
+connection = sqlite3.connect("MoviesFinder.db")
 
 # cursor for executing queries to the database
 cursor = connection.cursor()
-    
+
 
 def find_platforms(movie_name):
 
@@ -25,7 +20,17 @@ def find_platforms(movie_name):
 
     # check if movie exists
     if not table:
-        print(f"{movie_name} does not exist in our database")
+        print(f"{movie_name} does not exist in our database \n")
+        query = f"SELECT movies.name FROM movies;"
+        cursor.execute(query)
+        table = cursor.fetchall()
+        entries = []
+        for entry in table:
+            for platform in entry:
+                entries.append(platform)
+        print("Please select one of these movies from our database: \n")
+        print(f'\n'.join(entries))
+
     else:
         entries = []
         for entry in table:
@@ -43,9 +48,20 @@ def find_movies(platform_name):
     cursor.execute(query)
 
     table = cursor.fetchall()
+    
     # check if platform has movies
     if not table:
         print(f"{platform_name} does not have any movies in our database")
+        query = f"SELECT platforms.platform_name FROM platforms;"
+        cursor.execute(query)
+        table = cursor.fetchall()
+        entries = []
+        for entry in table:
+            for platform in entry:
+                entries.append(platform)
+        print("Please select one of these platforms from our database: \n")
+        print(f'\n'.join(entries))
+
     else:
         entries = []
         for entry in table:
