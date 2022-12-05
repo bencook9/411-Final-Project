ecdb4d7d15f6bb2980208d5273d46deed9d0c9d6
vietninh <vietquocninh@gmail.com>
2022-12-04 20:54:22 -0600

    Updated the sql queries to match the new files in the database.

--- Movies Finder/main.py
+++ Movies Finder/main.py
@@ -15,7 +15,8 @@ cursor = connection.cursor()
 
 def find_platforms(movie_name):
 
-    query = f"""select platforms.platform_name from movies join on_platform on movies.movieId = on_platform.movie_id 
+    query = f"""select platforms.platform_name from movies
+                 join on_platform on movies.movie_id = on_platform.movie_id 
                 join platforms on platforms.platform_id = on_platform.platform_id 
                 where movies.name ='{movie_name}';"""
     cursor.execute(query)
@@ -26,13 +27,17 @@ def find_platforms(movie_name):
     if not table:
         print(f"{movie_name} does not exist in our database")
     else:
-        return table
+        entries = []
+        for entry in table:
+            for platform in entry:
+                entries.append(platform)
+        return entries
 
 
 def find_movies(platform_name):
 
     query = f"""SELECT movies.name FROM movies
-                join on_platform on movies.movieId = on_platform.movie_id 
+                join on_platform on movies.movie_id = on_platform.movie_id 
                 join platforms on platforms.platform_id = on_platform.platform_id 
                 where platforms.platform_name ='{platform_name}';"""
     cursor.execute(query)
@@ -42,7 +47,11 @@ def find_movies(platform_name):
     if not table:
         print(f"{platform_name} does not have any movies in our database")
     else:
-        return table
+        entries = []
+        for entry in table:
+            for movie in entry:
+                entries.append(movie)
+        return entries
 
 
 answer = input("Enter [m] if you want to search a movie. Enter [p] if you wish to search a platform:\n")
@@ -50,19 +59,19 @@ answer = input("Enter [m] if you want to search a movie. Enter [p] if you wish t
 match answer.upper():
     case "M":
         movie_name = input("What movie would you like to search for?\n")
-        # MAKE CALL TO METHOD movie_name.lower()
-        platforms = find_platforms(movie_name)
+        platforms = find_platforms(movie_name.upper())
 
         if platforms:
-            print(platforms)
+            print(f"""\nHere are the platforms for the movie "{movie_name}":\n""")
+            print(f'\n'.join(platforms))
         
     case "P":
         platform_name = input("What platform would you like to search for?\n")
-        # MAKE CALL TO METHOD platform_name.lower()
-        movies = find_movies(platform_name)
+        movies = find_movies(platform_name.upper())
         
         if movies:
-            print(movies)
+            print(f"""\nHere are the movies from "{platform_name}":\n""")
+            print(f'\n'.join(movies))
 
     case _:
         print("Input not recognized ending program")
